8194d8c59da11df3445fadc2b692d2dfc7bd3076
dang ngoc son <sonever4@gmail.com>
2025-08-19 22:24:33 +0700

    add appbar and navigationbar

---
+++ lib/widgets/shared/navigationbar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const MyNavigationBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorites'),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+      ],
+      currentIndex: currentIndex,
+      selectedItemColor: Theme.of(context).colorScheme.primary,
+      onTap: onTap,
+      type: BottomNavigationBarType.fixed,
+    );
+  }
+}
