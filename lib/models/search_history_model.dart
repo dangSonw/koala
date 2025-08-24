5e26852f21a5452d05da11771a9d42f25594a3ab
dang ngoc son <sonever4@gmail.com>
2025-08-25 00:50:16 +0700

    nothing

--- lib/models/search_history_model.dart
+++ lib/models/search_history_model.dart
@@ -1,17 +1,13 @@
 import '../cache/storage_manager.dart';
 
-/// Wrapper around the `search_history` Hive box.
-/// Stores recent search queries as a simple list under a single key.
 class SearchHistoryBox {
   static const String _keyQueries = 'queries';
 
-  /// Returns all queries (most recent last).
   static List<String> getAll() {
     final list = StorageManager.searchHistoryBox.get(_keyQueries, defaultValue: <String>[]) as List<dynamic>;
     return list.map((e) => e.toString()).toList(growable: false);
   }
 
-  /// Adds a query to the end of the list. Removes duplicates by moving to the end.
   static Future<void> add(String query, {int maxLength = 50}) async {
     final list = List<String>.from(getAll());
     list.removeWhere((e) => e == query);
@@ -22,7 +18,6 @@ class SearchHistoryBox {
     await StorageManager.searchHistoryBox.put(_keyQueries, list);
   }
 
-  /// Removes the query at index.
   static Future<void> removeAt(int index) async {
     final list = List<String>.from(getAll());
     if (index >= 0 && index < list.length) {
@@ -31,7 +26,6 @@ class SearchHistoryBox {
     }
   }
 
-  /// Clears all queries.
   static Future<void> clear() async {
     await StorageManager.searchHistoryBox.put(_keyQueries, <String>[]);
   }
